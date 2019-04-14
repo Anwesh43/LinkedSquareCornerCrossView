d456d0512cfd44d0296e4941e6b25d65539b12c7
Anwesh43 <anweshthecool0@gmail.com>
2019-04-14 16:15:59 +0530

    adding SCCNode class

--- squarecornercrossview/src/main/java/com/anwesh/uiprojects/squarecornercrossview/SquareCornerCrossView.kt
+++ squarecornercrossview/src/main/java/com/anwesh/uiprojects/squarecornercrossview/SquareCornerCrossView.kt
@@ -130,4 +130,48 @@ class SquareCornerCrossView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SCCNode(var i : Int, val state : State = State()) {
+
+        private var next : SCCNode? = null
+        private var prev : SCCNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = SCCNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSCCNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SCCNode {
+            var curr : SCCNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
